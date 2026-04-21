fc5ddaa12730634510122344a2aac9e08c8dfeeb
Pavel Mikryukov <pavelmikryukov2001@gmail.com>
2026-04-21 15:59:45 +0400

    docker compose fix

--- Documents.WebApi/Program.cs
+++ Documents.WebApi/Program.cs
@@ -6,7 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddServices(builder.Configuration);
 
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
 var app = builder.Build();
+
+app.UseRouting();
+
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
@@ -16,6 +23,14 @@ if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
+
+app.Use(async (context, next) =>
+{
+    app.Logger.LogInformation("[REQUEST] {Method} {Path}", context.Request.Method, context.Request.Path);
+    await next();
+    app.Logger.LogInformation("[RESPONSE] {StatusCode}", context.Response.StatusCode);
+});
+
 
 app.AddEndPoints();
 
