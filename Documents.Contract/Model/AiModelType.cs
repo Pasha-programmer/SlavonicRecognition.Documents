bdfb2cda70aa3977019de81f3708d7fc979b2cf1
Pavel Mikryukov <pavelmikryukov2001@gmail.com>
2026-05-19 19:12:07 +0400

    Ф

--- Documents.Contract/Model/AiModelType.cs
+++ Documents.Contract/Model/AiModelType.cs
@@ -11,14 +11,17 @@ public enum AiModelType : int
     GlagoliticModelFull = 0,
 
     [Description("v1.1")]
-    GlagoliticModelFullV1_1 = 1,
+    GlagoliticModelFullV1_1 = 11,
 
     [Description("v2.0")]
-    GlagoliticModelFullV2_0 = 2,
+    GlagoliticModelFullV2_0 = 20,
 
     [Description("v2.1")]
-    GlagoliticModelFullV2_1 = 3,
-    
+    GlagoliticModelFullV2_1 = 21,
+
+    [Description("v2.2")]
+    GlagoliticModelFullV2_2 = 22,
+
     [Description("v3.0")]
-    GlagoliticModelFullV3_0 = 4,
+    GlagoliticModelFullV3_0 = 30,
 }
